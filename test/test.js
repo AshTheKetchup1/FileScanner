1cc2318f15969c2c4ed73df1809f87dd135af912
Ashley Dang <ashketchup@sbcglobal.net>
2016-05-14 22:57:56 -0500

    added other add and subtract tests

--- test/test.js
+++ test/test.js
@@ -10,14 +10,21 @@ describe('canary', function () {
 		expect(VirusTotal.foo()).to.equal("canary");
 	});
 });
+
 describe('VirusTotal.add', function () {
 	it('test add function', function(){
 		expect(VirusTotal.add(2,3)).to.equal(5);
+	});
+	it('test add function 2', function(){
+		expect(VirusTotal.add(-20,25)).to.equal(5);
 	});
 });
 
 describe('VirusTotal.subtract', function () {
 	it('test subtract function', function(){
 		expect(VirusTotal.subtract(3,2)).to.equal(1);
+	});
+	it('the other subtract function I guess', function(){
+		expect(VirusTotal.subtract(-20,25)).to.equal(-45);
 	});
 });
