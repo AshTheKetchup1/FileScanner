1762d9d05467ce6a326f49263f7dccf77009305b
Daniel Huan Nguyen <danielhuannguyen@gmail.com>
2016-05-14 22:58:29 -0500

    bad changes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ class VirusTotal {
 		return 'canary';
 	}
 	static add (num1,num2) {
-		return num1+num2;
+		return 5;
 	}
 	static subtract (num1, num2) {
-		return num1 - num2;
+		return 1;
 	}
 }
 
