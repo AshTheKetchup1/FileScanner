b1e0d06d70685d589657aac12195fdae74e4cbaf
Ashley Dang <ashketchup@sbcglobal.net>
2016-05-21 22:30:08 -0500

    VirusTotal lesson

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-class VirusTotal {
-	static foo () {
-		return 'canary';
-	}
-	static add (num1,num2) {
-		return num1+num2;
-	}
-	static subtract (num1, num2) {
-		return num1-num2;
-	}
-}
+var request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Show the HTML for the Google homepage.
+//   }
+// });
 
-module.exports = VirusTotal;
+
+var options = {
+	url:'https://www.virustotal.com/vtapi/v2/file/report',
+	form: {
+		apikey:'apikey',
+		resource: '52d3df0ed60c46f336c131bf2ca454f73bafdc4b04dfa2aea80746f5ba9e6d1c'
+	}
+};
+request.post(options, function(err,httpResponse,body){
+	if (!err && httpResponse.statusCode == 200) {
+    console.log(JSON.parse(body));
+  }
+});
+
+// class VirusTotal {
+// 	static foo () {
+// 		return 'canary';
+// 	}
+// 	static add (num1,num2) {
+// 		return num1+num2;
+// 	}
+// 	static subtract (num1, num2) {
+// 		return num1-num2;
+// 	}
+// }
+//
+// module.exports = VirusTotal;
--- test/test.js
+++ test/test.js
@@ -1,30 +1,30 @@
 var expect = require('chai').expect;
 var VirusTotal = require('../src/index.js');
 
-describe('canary', function () {
-	it ('canary test', function () {
-		expect(1).to.equal(1);
-	});
-
-	it ('canary function', function () {
-		expect(VirusTotal.foo()).to.equal("canary");
-	});
-});
-
-describe('VirusTotal.add', function () {
-	it('test add function', function(){
-		expect(VirusTotal.add(2,3)).to.equal(5);
-	});
-	it('test add function 2', function(){
-		expect(VirusTotal.add(-15,25)).to.equal(10);
-	});
-});
-
-describe('VirusTotal.subtract', function () {
-	it('test subtract function', function(){
-		expect(VirusTotal.subtract(3,2)).to.equal(1);
-	});
-	it('the other subtract function I guess', function(){
-		expect(VirusTotal.subtract(-20,25)).to.equal(-45);
-	});
-});
+// describe('canary', function () {
+// 	it ('canary test', function () {
+// 		expect(1).to.equal(1);
+// 	});
+//
+// 	it ('canary function', function () {
+// 		expect(VirusTotal.foo()).to.equal("canary");
+// 	});
+// });
+//
+// describe('VirusTotal.add', function () {
+// 	it('test add function', function(){
+// 		expect(VirusTotal.add(2,3)).to.equal(5);
+// 	});
+// 	it('test add function 2', function(){
+// 		expect(VirusTotal.add(-15,25)).to.equal(10);
+// 	});
+// });
+//
+// describe('VirusTotal.subtract', function () {
+// 	it('test subtract function', function(){
+// 		expect(VirusTotal.subtract(3,2)).to.equal(1);
+// 	});
+// 	it('the other subtract function I guess', function(){
+// 		expect(VirusTotal.subtract(-20,25)).to.equal(-45);
+// 	});
+// });
